bfaffc6587b204433e271867c7646f87f00621df
Harland Hauger & John Cech <student@epicodus.com>
2014-04-07 11:48:00 -0700

    have funactionality for checkboxes

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -5,10 +5,14 @@ class LinesController < ApplicationController
 
   def new
     @line = Line.new
+    @stations = Station.all
   end
 
   def create
-    @line = Line.create(params[:Line])
+    @line = Line.create(params[:line])
+    params[:station_ids].each do |station_id|
+      Stop.create({:line_id => @line.id, :station_id => station_id})
+    end
     flash[:notice] = "#{@line.name} has been added"
     redirect_to "/lines/"
   end
@@ -19,11 +23,15 @@ class LinesController < ApplicationController
 
   def edit
     @line = Line.find(params[:id])
+    @stations = Station.all
   end
 
   def update
     @line = Line.find(params[:id])
-    @line.update(params[:Line])
+    params[:station_ids].each do |station_id|
+      Stop.create({:line_id => @line.id, :station_id => station_id})
+    end
+    @line.update(params[:line])
     flash[:notice] = "#{@line.name} Updated"
     redirect_to "/lines"
 
@@ -33,7 +41,7 @@ class LinesController < ApplicationController
     @line = Line.find(params[:id])
     @line.destroy
     flash[:notice] = "#{@line.name} has been Removed"
-    redirect_to "/"
+    redirect_to "/lines"
   end
 end
 
